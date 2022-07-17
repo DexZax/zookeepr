64b7ac1d07564b6331a1e31eae2cb16ec3d2f0fa
Dexter Zacour <cdzacour@hotmail.com>
2022-07-17 15:49:35 -0400

    finished heroku set up

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const { animals } = require('./data/animals');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -52,8 +54,7 @@ function filterByQuery(query, animalsArray) {
     res.json(results);
   });
 
-  
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
-  
